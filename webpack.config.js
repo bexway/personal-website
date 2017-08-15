6f9d8fd5bf59b336c0ba8805a638832cde240aac
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-08-14 22:29:07 -0500

    master - initial work from intro

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
