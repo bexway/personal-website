2d799ef0c4ed2b8b03401b142b0418b321fc2774
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-08-17 14:16:22 -0500

    master - changed html skeleton arrangement, added plugins, added font file

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        require('autoprefixer'),
-        require('cssnext')
-
+        require('postcss-cssnext'),
+        require('precss')
     ]
 }
