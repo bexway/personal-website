22e7b2847855a386a143e81ddfd86e5bfe78e92e
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-08-18 22:05:52 -0500

    master - added slugignore, server, and routes to host with express, and fixed an image link

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var path = require("path");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(port);
