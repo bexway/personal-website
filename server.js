d169dbe80905ecb9daa95755a921b2c129b5c352
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-08-19 16:59:16 -0500

    master - fixing static dist

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-// app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 require('./routes/htmlroutes.js')(app);
 
