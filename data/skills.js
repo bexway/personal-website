a350af0de2861640b3d842ce0264c3db3d55d491
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-06 01:19:46 -0500

    handlebars - added skills data and iterator

---
+++ data/skills.js
@@ -0,0 +1,31 @@
+var skills = [
+    {
+        skillName: "HTML5",
+        skillRelated: []
+    },
+    {
+        skillName: "CSS3",
+        skillRelated: []
+    },
+    {
+        skillName: "Javascript",
+        skillRelated: [
+            "Jquery"
+        ]
+    },
+    {
+        skillName: "Node.js",
+        skillRelated: ["Express", "Express Handlebars"]
+    },
+    {
+        skillName: "Python",
+        skillRelated: []
+    },
+    {
+        skillName: "MatLab",
+        skillRelated: []
+    }
+
+]
+
+module.exports = skills;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,14 @@
 var path = require('path');
 var express = require("express");
 var projects = require("../data/projects.js");
+var skills = require("../data/skills.js");
 
 var router = express.Router();
 
 router.get("/", function(req, res) {
   var indexObj = {
-    projects: projects
+    projects: projects,
+    skills: skills
   };
   res.render("index", indexObj);
 });
