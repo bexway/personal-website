e86865bcdda7e89a65d58cb31d3d01caf15b8fdf
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-26 18:42:33 -0500

    page-reformat - added handlebars and routes for projects and skills pages

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -5,7 +5,21 @@ var skills = require("../data/skills.js");
 
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/projects", function(req, res) {
+  var projectsObj = {
+    projects: projects,
+  };
+  res.render("projects", projectsObj);
+});
+
+router.get("/skills", function(req, res) {
+  var skillsObj = {
+    skills: skills
+  };
+  res.render("skills", skillsObj);
+});
+
+router.use(function(req, res) {
   var indexObj = {
     projects: projects,
     skills: skills
