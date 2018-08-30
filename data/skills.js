a6e9e006cbc5a14ea2b926c4550e5b777ae37fe0
Bex Way <bexzway@gmail.com>
2018-08-29 22:51:33 -0500

    Updated skills

--- data/skills.js
+++ data/skills.js
@@ -1,45 +1,46 @@
 var skills = [
     {
+        skillName: "Python",
+        skillRelated: [
+            "PySpark",
+            "Unittest and Mock"
+        ]
+    },
+    {
+        skillName: "Relational Databases",
+        skillRelated: [
+            "MySQL",
+            "Snowflake Data Warehouse"
+        ]
+    },
+    {
+        skillName: "AWS Cloud Services",
+        skillRelated: []
+    },
+    {
+        skillName: "Agile Work Environment",
+        skillRelated: []
+    },
+    {
         skillName: "HTML5",
         skillRelated: []
     },
     {
         skillName: "CSS3",
-        skillRelated: [
-            "PostCSS"
-        ]
+        skillRelated: []
     },
     {
         skillName: "Javascript",
-        skillRelated: [
-            "Jquery",
-            "React"
-        ]
+        skillRelated: []
     },
     {
         skillName: "Node.js",
         skillRelated: ["Express", "Express Handlebars"]
     },
     {
-        skillName: "Python",
-        skillRelated: []
-    },
-    {
-        skillName: "SQL",
-        skillRelated: []
-    },
-    {
         skillName: "NoSQL",
-        skillRelated: [
-            "Mongo DB",
-            "Mongoose"
-        ]
-    },
-    {
-        skillName: "MatLab",
         skillRelated: []
     }
-
 ]
 
 module.exports = skills;
